de93d475f0df4988e0e44d710a8f43fc268c8e67
Alex <alexandepp@gmail.com>
2021-10-05 21:46:08 +0300

    homework 5pcs

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import s from './pages/hm5.module.css'
+import {PATH} from "./Routes";
+
 
 function Header() {
     return (
-        <div>
-            <NavLink to='/'>Home </NavLink>
-            <NavLink to='PreJunior'>PreJunior </NavLink>
-            <NavLink to='Junior'>Junior </NavLink>
-            <NavLink to='Junior+'>Junior+</NavLink>
-
+        <div >
+            <div className={s.headerNavbar}>
+                <NavLink to={PATH.PRE_JUNIOR}activeClassName={s.activeNav} className={s.navbar} >PreJunior </NavLink>
+                <NavLink to={PATH.JUNIOR} activeClassName={s.activeNav} className={s.navbar}>Junior </NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.activeNav} className={s.navbar}>Junior+</NavLink>
+                <div className={s.button}>Push me</div>
+            </div>
         </div>
     )
 }
 
 export default Header
+
+
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import s from ''
+import s from './hm5.module.css'
 
 function Error404() {
     return (
         <div className={s.error}>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            {/*<div>404</div>*/}
+            {/*<div>Page not found!</div>*/}
+            {/*<div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
         </div>
     )
 }
