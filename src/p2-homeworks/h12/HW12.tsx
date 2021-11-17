65be74fe4b709b6555e6f857809b7cd3e40c7efb
Alex <alexandepp@gmail.com>
2021-11-17 20:18:47 +0300

    homework 12pcs finish

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -8,37 +8,66 @@ const themes = ['dark', 'red', 'some'];
 
 function HW12() {
     debugger
-    const state = useSelector<AppStoreType,initStateType>(state => state.themeReducer)
+    const state = useSelector<AppStoreType, initStateType>(state => state.themeReducer)
     const dispatch = useDispatch()
     const theme = state.value; // useSelector
 
-
-    const onChangeCallback = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeCallback = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(darkThemeAC(e.currentTarget.value))
-        // e.target.value === 'dark' ? dispatch(darkThemeAC('THEME'))
-        console.log(e.target.value)
     }
-    // useDispatch, onChangeCallback
 
     return (
         <div className={s[theme]}>
             <hr/>
             <span className={s[theme + '-text']}>
                 homeworks 12
-
             </span>
-            <span>dark</span>
+            <div className={s.block}>
+            <div>
+                <span>dark</span>
+                <input type='radio'
+                       name='change'
+                       value='dark'
+                       onChange={onChangeCallback}
+                />
+            </div>
+            <div>
+                <span>light</span>
+                <input type='radio'
+                       name='change'
+                       value='light'
+                       onChange={onChangeCallback}
+                />
+            </div>
+            <div>
+                <span>red</span>
+                <input type='radio'
+                       name='change'
+                       value='red'
+                       onChange={onChangeCallback}
+                />
+            </div>
+            <span>red-text</span>
             <input type='radio'
                    name='change'
-                   value='DARK'
+                   value='red-text'
                    onChange={onChangeCallback}
             />
-            <span>light</span>
+            <div>
+            <span>some</span>
             <input type='radio'
                    name='change'
-                   value='LIGHT'
+                   value='some'
                    onChange={onChangeCallback}
             />
+                </div>
+            <span>some-text</span>
+            <input type='radio'
+                   name='change'
+                   value='some-text'
+                   onChange={onChangeCallback}
+            />
+            </div>
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,85 +1,27 @@
+
 export const initState = {
     value: 'light'
 };
 export type initStateType = {
     value: string
 }
-
 type ThemeACType = {
-    type: 'DARK' | 'LIGHT' | 'RED' | 'RED-TEXT' | 'SOME' | 'SOME-TEXT'
+    type: 'THEME'
     value: string
 }
 
-// type lightThemeACType = {
-//     type: 'THEME-LIGHT'
-//     value: string
-// }
-// type redThemeACType = {
-//     type: 'THEME-RED'
-//     value: string
-// }
-// type redTextThemeACType = {
-//     type: 'THEME-RED-TEXT'
-//     value: string
-// }
-// type someThemeThemeACType = {
-//     type: 'THEME-SOME'
-//     value: string
-// }
-// type someTextACType = {
-//     type: 'THEME-SOME-TEXT'
-//     value: string
-// }
 
 type ActionTypes =  ThemeACType
-    // | lightThemeACType |
-    // redThemeACType | redTextThemeACType |
-    // someThemeThemeACType | someTextACType
+
 
 
 export const themeReducer = (state = initState, action: ActionTypes): typeof state => { // fix any
     switch (action.type) {
-        // case "THEME-LIGHT": {
-        //     return {
-        //         ...state,
-        //         value: action.value
-        //     };
-        // }
-        case 'DARK': {
+        case 'THEME': {
           return {
               ...state,
               value: action.value
           }
-        }
-        case 'LIGHT': {
-            return {
-                ...state,
-                value: action.value
-            }
-        }
-        case 'RED': {
-            return {
-                ...state,
-                value: action.value
-            }
-        }
-        case 'RED-TEXT': {
-            return {
-                ...state,
-                value: action.value
-            }
-        }
-        case 'SOME': {
-            return {
-                ...state,
-                value: action.value
-            }
-        }
-        case 'SOME-TEXT': {
-            return {
-                ...state,
-                value: action.value
-            }
         }
         default: return state;
     }
@@ -90,20 +32,5 @@ export const changeThemeC = (): any => {}; // fix any
 
 
 export const darkThemeAC = (value: string): ThemeACType => {
-    return( {type: 'DARK', value})
-}
-export const lightThemeAC = (value: string): ThemeACType => {
-    return( {type: 'LIGHT', value})
-}
-export const redThemeAC = (value: string): ThemeACType => {
-    return( {type: 'RED', value})
-}
-export const redTextThemeAC = (value: string): ThemeACType => {
-    return( {type: 'RED-TEXT', value})
-}
-export const someThemeAC = (value: string): ThemeACType => {
-    return( {type: 'SOME', value})
-}
-export const someTextThemeAC = (value: string): ThemeACType => {
-    return( {type: 'SOME-TEXT', value})
+    return( {type: 'THEME', value})
 }
