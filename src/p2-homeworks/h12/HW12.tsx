c2e39dc13f3228530fb71656b503fce129d307f6
Alex <alexandepp@gmail.com>
2021-11-17 19:42:55 +0300

    homework 12pcs finish

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {ChangeEventHandler} from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {darkThemeAC, initStateType} from "./bll/themeReducer";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+    debugger
+    const state = useSelector<AppStoreType,initStateType>(state => state.themeReducer)
+    const dispatch = useDispatch()
+    const theme = state.value; // useSelector
 
+
+    const onChangeCallback = (e:React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(darkThemeAC(e.currentTarget.value))
+        // e.target.value === 'dark' ? dispatch(darkThemeAC('THEME'))
+        console.log(e.target.value)
+    }
     // useDispatch, onChangeCallback
 
     return (
@@ -13,8 +25,20 @@ function HW12() {
             <hr/>
             <span className={s[theme + '-text']}>
                 homeworks 12
-            </span>
 
+            </span>
+            <span>dark</span>
+            <input type='radio'
+                   name='change'
+                   value='DARK'
+                   onChange={onChangeCallback}
+            />
+            <span>light</span>
+            <input type='radio'
+                   name='change'
+                   value='LIGHT'
+                   onChange={onChangeCallback}
+            />
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
